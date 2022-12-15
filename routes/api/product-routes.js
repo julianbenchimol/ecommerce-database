a034886de337d381c42e43d0f4cbfc969295af74
Julian Benchimol <jbenchimol1999@gmail.com>
2022-12-15 11:35:14 -0500

    product routes created

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -5,14 +5,23 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // get all products
 router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+  Product.findAll().then((productData) =>{
+    res.json(productData)
+  })
 });
 
 // get one product
-router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+router.get('/:id', async (req, res) => {
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: [{ model: Category, Tag}]
+    });
+    res.status(200).json(productData)
+  }
+  catch(err){
+    res.status(500).json(err)
+  }
+
 });
 
 // create new product
@@ -90,7 +99,15 @@ router.put('/:id', (req, res) => {
 });
 
 router.delete('/:id', (req, res) => {
-  // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then((deletedProduct) =>{
+    res.json(deletedProduct)
+  })
+  .catch((err) => res.json(err))
 });
 
 module.exports = router;
